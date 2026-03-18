de6367b7ab68fcd0df65adbf0a4adf2b09496ead
Yin Changling <changling.yin@keysocapp.com>
2026-03-18 17:44:10 +0800

    [feature] Change domain name

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,5 +65,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.hosts << "www.hiphop.com"
+  config.hosts << "www.hiphop.top"
 end
